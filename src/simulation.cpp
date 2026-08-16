4d37923dee5d20c3b43d51d066b4b41a23c0d46c
Mr-T18 <itsuki.tkhs@gmail.com>
2026-08-16 18:13:34 +0900

    回文式で全ステップ幅でRK4を適用するための2重while文のアプローチ(未作成)

--- src/simulation.cpp
+++ src/simulation.cpp
@@ -5,6 +5,12 @@
 #include "constants.hpp"
 #include "integrator.hpp"
 
+inline void output_state(std::ofstream ofs, double t, const State& sat) {
+  ofs << std::scientific << std::setprecision(15) << t << " " << sat.r.x << " "
+      << sat.r.y << " " << sat.r.z << " " << sat.v.x << " " << sat.v.y << " "
+      << sat.v.z << "\n";
+}
+
 int main() {
   // 初期条件の設定
   State sat = {{physics::x0, physics::y0, physics::z0},
@@ -17,29 +23,45 @@ int main() {
   std::cout << "Start Hill Simulation" << std::endl;
 
   double t = 0.0;
-  const double OUTPUT_INTERVAL = 1e-3;  // 出力間隔
+  const double OUTPUT_INTERVAL = 1e-3;  // 出力間隔[年]
   double next_output_time = 0.0;
 
+  // 初期位置，初速度の出力
+  ofs << std::scientific << std::setprecision(15) << t << " " << sat.r.x << " "
+      << sat.r.y << " " << sat.r.z << " " << sat.v.x << " " << sat.v.y << " "
+      << sat.v.z << "\n";
+  next_output_time += OUTPUT_INTERVAL;
+
+  // 最初に半ステップだけガス抗力項で速度のみ進める
+  sat.v = rk4_step(sat, physics::DT * 0.5);
+
   while (t <= physics::MAX_YEARS) {
-    // データ出力
-    if (t >= next_output_time - 1e-9) {
-      ofs << std::scientific << std::setprecision(15) << t << " " << sat.r.x
-          << " " << sat.r.y << " " << sat.r.z << " " << sat.v.x << " "
-          << sat.v.y << " " << sat.v.z << "\n";
-      next_output_time += OUTPUT_INTERVAL;
-    }
+    t += physics::DT_YEARS;
 
     // 回文式での更新
-    // ガス抗力項で速度をdt/2だけ更新
-    sat.v = rk4_step(sat, physics::DT * 0.5);
-
     // 保存力・コリオリ力項で位置と速度をリープフロッグ法で更新
     sat = leapfrog_step(sat, physics::DT);
 
-    // 最後にガス抗力項で速度をもう一度半ステップ分更新
-    sat.v = rk4_step(sat, physics::DT * 0.5);
+    bool is_last_step = (t > physics::MAX_YEARS - physics::DT_YEARS);
+    if (is_last_step) {
+      // 最後にガス抗力項で速度をもう一度半ステップ分更新
+      sat.v = rk4_step(sat, physics::DT * 0.5);
+    } else {
+      // ガス抗力項で全ステップ分更新
+      sat.v = rk4_step(sat, physics::DT);
+    }
 
-    t += physics::DT_YEARS;
+    // データ出力
+    if (t >= next_output_time - 1e-9) {
+      State output_sat = sat;
+      output_sat.v = rk4_step(sat, physics::DT * 0.5);
+
+      ofs << std::scientific << std::setprecision(15) << t << " "
+          << output_sat.r.x << " " << output_sat.r.y << " " << output_sat.r.z
+          << " " << output_sat.v.x << " " << output_sat.v.y << " "
+          << output_sat.v.z << "\n";
+      next_output_time += OUTPUT_INTERVAL;
+    }
   }
 
   std::cout << "Simulation Finish!" << std::endl;
